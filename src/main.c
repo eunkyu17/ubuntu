cfec5158a97a21610a50091ea0f9d8158a891bec
eunkyu17 <eunkyu17i@naver.com>
2025-03-24 16:28:34 +0900

    d

--- src/main.c
+++ src/main.c
@@ -1,77 +1,80 @@
-#include "lcd.h"
+// SHT 5초 마다 한번씩 온습도 측정 - timer0 5초 주기를 계산.
+// 외부 EEPROM  의 주소는 0100-> 온도 0200-> 습도
+// SHT 에서 측정 실패는 error 성공 시에만 EEPROM 에 저장.
+// INT4 번 써서(스위치를 누르면) EEPROM 에 데이터를 읽어서 UART로 출력하기.
+// 0 ~ 99.9 표현하려면 1 byte 로는 부족하다. ???
+
+#include "SHT2x.h"
+#include "at25160.h"
 #include "uart0.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <stdio.h>
 
-volatile uint8_t intData = '0';
-uint8_t cursor = 0;
+volatile uint8_t readFlag = 1, txFlag = 0;
+uint16_t timerCount = 0;
+uint16_t temperatureC, humidityRH;
 
-int main()
+int main(void)
 {
+    SPI_Init();
+
+    Init_TWI();
+    SHT2x_Init();
+
     uart0Init();
-    lcdInit();
+    DDRE = _BV(PE1); // 0x02;
 
-    stdin = &INPUT;
+    // interrupt 4 설정
+    EICRB = 0x03; // int 4 상승 엣지
+    EIMSK = 0x10;
+
+    TCCR0 = 0x07; // 1024 분주비
+    TCNT0 = 112;  // 144 세기.. 16M /1024/ 0.1초 ....
+    TIMSK = 0x01; // timer0 ovf enable
+
+    sei();
     stdout = &OUTPUT;
-
-    DDRE = 0x02; // Rx(입력), TX(출력)1, SW0~3 입력
-
-    EICRB = 0xFF; // 4567 상승 엣지에서 동작 289p.
-    EIMSK = 0xF0; // 4567 허용
-    EIFR = 0xF0;  // 4567 클리어
-
-    sei(); // 전역 인터럽트 허용
-    char cData;
-
-    printf("Hi, I'm Atmega128");
 
     while (1)
     {
-        if (intData != '0')
+        if (readFlag)
         {
-            printf("\n\r Input Switch : %c", intData);
-            intData = '0';
+            // i2c temp read -> spi eeprom write
+            SHT2x_MeasureHM(TEMP, (nt16 *)&temperatureC);
+            SHT2x_MeasureHM(HUMIDITY, (nt16 *)&humidityRH);
+            temperatureC = SHT2x_CalcTemperatureC(temperatureC) * 10;
+            humidityRH = SHT2x_CalcRH(humidityRH) * 10;
+            at25160_Write_Arry(0x0100, (uint8_t *)&temperatureC, 2);
+            at25160_Write_Arry(0x0200, (uint8_t *)&humidityRH, 2);
+            readFlag = 0;
         }
-        // TODO : 4개 문자 이상 못 받아 오는 문제 버퍼 문제 해결 s
-        // cData = fgetc(stdin);
-        lcdDataWrite(cData);
-        cursor++;
-        if (cursor == 16)
-            lcdGotoXY(0, 1);
-        else if (cursor >= 32)
+        if (txFlag)
         {
-            cursor = 0;
-            lcdGotoXY(0, 0);
+            // eeprom read -> uart printf();
+            at25160_Read_Arry(0x0100, (uint8_t *)&temperatureC, 2);
+            at25160_Read_Arry(0x0200, (uint8_t *)&humidityRH, 2);
+            uart0PrintString("\n\rTemp: ");
+            printf("%u.%u", temperatureC / 10, temperatureC % 10);
+            uart0PrintString("\n\rHumi: ");
+            printf("%u.%u", humidityRH / 10, humidityRH % 10);
+            txFlag = 0;
         }
     }
     return 0;
 }
 
+ISR(TIMER0_OVF_vect)
+{
+    timerCount++;
+    if (timerCount >= 500) // 5초 확인
+    {
+        timerCount = 0;
+        readFlag = 1;
+    }
+    TCNT0 = 112;
+}
+
 ISR(INT4_vect)
 {
-    cli();
-    intData = '1';
-    sei();
-}
-
-ISR(INT5_vect)
-{
-    cli();
-    intData = '2';
-    sei();
-}
-
-ISR(INT6_vect)
-{
-    cli();
-    intData = '3';
-    sei();
-}
-
-ISR(INT7_vect)
-{
-    cli();
-    intData = '4';
-    sei();
+    txFlag = 1;
 }
