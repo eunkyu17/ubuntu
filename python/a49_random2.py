5b1b2c49d23951335e64b507bd54f97ea892d53b
eunkyu17 <eunkyu17i@naver.com>
2025-05-12 15:10:44 +0900

    a21_cPython_import.py 파일 삭제 및 a49_random2.py, a50_sys.py, a51_os.py, a52_urllib.py 파일 추가

--- python/a49_random2.py
+++ python/a49_random2.py
@@ -11,22 +11,29 @@ import numpy as np
 
 
 def main():
-    n_samples = 100000
-    sample_size = 300
+    n_samples = 10000
+    sample_size = 30
     a = 0
     b = 1
 
     means = []
-
     # uniform 균등 분포 결과 테스트
+    for _ in range(n_samples):
+        value = random.uniform(a, b)
+        means.append(value)
+    plt.hist(means, bins=sample_size)
+    plt.show()
+
+    means = []
+    # uniform 균등 분포 평균결과 테스트
     for _ in range(n_samples):
         sample = [random.uniform(a, b) for _ in range(sample_size)]
         mean = sum(sample) / sample_size
         means.append(mean)
     print("표준 편차: ", np.std(means))
     plt.hist(means, bins=sample_size)
-
     plt.show()
+
     # gauss() 함수 테이터
     mu = (a + b) / 2
     sigma = math.sqrt((b - a) ** 2 / (12 * sample_size))  # 표준 편차
